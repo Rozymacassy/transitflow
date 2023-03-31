37a7c4dc43284e9d0ecacae3f8fb01582501d47f
Rozymacassy <mariposamacassy@gmail.com>
2023-03-31 17:44:59 +0100

    modified

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,6 +3,9 @@ import './About.css'
 import IMG1 from '../../assets/container.png'
 import IMG2 from '../../assets/plane-cut.png'
 import IMG3 from '../../assets/lady.png'
+import IMG4 from '../../assets/box.png'
+import IMG5 from '../../assets/ticket.png'
+
 
 
 
@@ -14,7 +17,7 @@ const About = () => {
         <img src={IMG1} alt="" />
 
         <div className="child-container">
-          <div className="row">
+          <div className="row child_container">
             <div className="col-md-6 ">
               <p className='abt-text1'>Why Us</p>
               <h2>
@@ -26,18 +29,46 @@ const About = () => {
               <p  className='abt-text2'> Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.
 
               </p>
-             
-              
+              <div className="delivery">
+                <div className="box d-flex mb-3">
+                <img src={IMG4} className="box-icon " alt="" />
+                <p>Delivery on Time</p>
+                </div>
+               
+                
+                <div className='ticket d-flex'>
+                <img src={IMG5} className="ticket-icon" alt="" />
+                <p>Optimized Travel Cost</p>
+                </div>
+              </div>
+      
 
             </div>
 
             <div className="col-md-6 plane-pic">
-              <img src={IMG2} alt="" />
+              <div>
+                  <img src={IMG2} alt="" />
+
+              </div>
 
               <div className='lady-pic'>
                 <img src={IMG3} className='lady'alt="" />
               </div>
             </div>
+          </div>
+
+
+          <div className='d-flex lines'>
+           <div className="col-md-6 " >
+            <h2 className='nos'>1294 <span>Delivered Packages</span> </h2>
+
+           </div>
+           
+           <div className="col-md-6">
+           <h2 className='nos1'>3594 <span>Satisfied Clients</span> </h2>
+           
+           </div>
+            
           </div>
         </div>
       </div>
--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import './Project.css'
+import IMG1 from '../../assets/tanker-back.png'
+import IMG2 from '../../assets/man-lady1.png'
+import IMG3 from '../../assets/trailler-front.png'
+import IMG4 from '../../assets/cont-road.png'
+import IMG5 from '../../assets/sea-ship.png'
 
 
 const Project = () => {
   return (
-    <div>
-      
-      
-    </div>
+    <section id='project'>
+      <div className='container project-container'> 
+           <h3>Transporting Across The World</h3>
+
+        <div className="row">
+         <div className="col-md-2"><img src={IMG1} alt="" /></div>
+         <div className="col-md-2"><img src={IMG2} alt="" /></div>
+         <div className="col-md-2"><img src={IMG3} alt="" /></div>
+         <div className="col-md-2"><img src={IMG4} alt="" /></div>
+         <div className="col-md-2"><img src={IMG5} alt="" /></div>  
+        </div>
+  
+            
+      </div>
+    </section>
+    
   )
 }
 
